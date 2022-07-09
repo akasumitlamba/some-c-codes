5abb2f695f33f24afac0ee5b573888ce3d50cd91
sumit kumar <85924848+akasumitlamba@users.noreply.github.com>
2022-07-09 22:47:19 +0530

    Update matrixtranspose.c

--- matrixtranspose.c
+++ matrixtranspose.c
@@ -9,7 +9,6 @@ void transpose(int x[][3])
 	{
 		for (j=0;j<3;++j)  // inner loop for columns of x
 {
-
 		y[i][j]=x[j][i];    // assigning transpose values into y
 } // end of inner loop
 	} // end of outer loop
