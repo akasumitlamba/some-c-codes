120c36e54a1a791c976a178562d1068bb5f86452
sumit kumar <85924848+akasumitlamba@users.noreply.github.com>
2022-08-15 22:10:57 +0530

    Update matrixtranspose.c

--- matrixtranspose.c
+++ matrixtranspose.c
@@ -1,5 +1,6 @@
 // Write a program that declares and initializes a matrix. Pass this matrix to a function that stores the transpose of this matrix into another matrix. Display the resultant matrix
 #include <stdio.h>
+
 /* following is the definition of the function transpose()
 that receives input array passed from the calling function */
 void transpose(int x[][3])
