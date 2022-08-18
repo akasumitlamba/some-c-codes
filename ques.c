c49f33244e6f589e1d30c9bc19a03f9145fd38bd
sumit kumar <kumara4749@gmail.com>
2022-08-19 01:58:33 +0530

    coomit

--- ques.c
+++ ques.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 int main()
+// {
+//     int arr[]={10,20,30,20,90,87,87,22},i;
+//     for (i=0;i<10;++i)
+//     {
+//         if(arr[i]==20)
+//         printf("\n successfully searched");
+//     }
+//     if(i==10)
+//     printf("search failed");
+// }
+
 {
-    int arr[]={10,20,30,20,90,87,87,22},i;
-    for (i=0;i<10;++i)
-    {
-        if(arr[i]==20)
-        printf("\n successfully searched");
-    }
-    if(i==10)
-    printf("search failed");
+    int x=900,*xp;
+    xp=&x;
+    printf("%d\n",*xp)
 }
